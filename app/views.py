48754be93b7407b3a178d39b86a622bce08bd66c
drDrozio <ishanss2018@gmail.com>
2021-09-07 12:29:17 +0530

    Backend API first release

--- app/views.py
+++ app/views.py
@@ -53,22 +53,11 @@ class RatingViewSet(viewsets.ModelViewSet):
     
 
     def create(self, request, *args, **kwargs):
-        # message
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        response = {'message':'Can not create like this'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        response = {'message':'Can not create like this'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-        return Response(serializer.data)
+        
